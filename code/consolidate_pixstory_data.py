b22764cc26711ba74223a2b21855e6d4f45c78de
ebenGunadi <egunadi@medinformatix.com>
2023-04-01 02:27:36 -0700

    Add code/rtg.py

--- code/consolidate_pixstory_data.py
+++ code/consolidate_pixstory_data.py
@@ -1,4 +1,5 @@
 import pandas as pd 
+from collections import defaultdict
 
 def convert_tsv_to_csv(input_file, output_file):
     input_path = '../data/pixstory/' + input_file
@@ -12,16 +13,30 @@ def get_clean_narratives():
     pixstory_filepath = '../data/pixstory/pixstory_langdetect.csv'
     pixstory_df = pd.read_csv(pixstory_filepath, delimiter=',', encoding='utf-8')
     
+    
     pixstory_df = pixstory_df[  (pixstory_df['tika_lan_code'] != 'en')
                               & (pixstory_df['ggl_lan_code'] != 'en') ]
     
     narrative_df = pixstory_df[['Story Primary ID', 'Narrative']]
+    narrative_df = narrative_df.rename(columns={'Story Primary ID': 'ID',
+                                                'Narrative': 'text'})
     
     # remove non-alphanumeric characters
-    narrative_df['Narrative'] = narrative_df['Narrative'].str.replace('[^0-9a-zA-Z.,-/ ]', '')
+    narrative_df['text'] = narrative_df['text'] \
+                            .astype(str) \
+                            .str.replace('[^0-9a-zA-Z.,-/ ]', '') \
+                            
+    # split narratives into 100-character chunks
+    narrative_dict = defaultdict(dict)
+    for narrative in narrative_df.itertuples():
+        text_length = len(narrative.text)
+        chunks_list = [narrative.text[i:i+100] 
+                        for i in range(0, text_length, 100)]
+        narrative_dict[narrative.ID] = chunks_list
     
+    narrative_df = pd.DataFrame(narrative_dict.items(), columns=['Story Primary ID', 'narrative_chunks'])
     narrative_df.to_csv('../data/pixstory/clean_narratives.csv', encoding='utf-8', index=False)
 
 if __name__ == '__main__':
-    # convert_tsv_to_csv('pixstory_v2.tsv', 'pixstory_v2.csv')
+    convert_tsv_to_csv('pixstory_v2.tsv', 'pixstory_v2.csv')
     get_clean_narratives()
