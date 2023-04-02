125afcbe9ca5caaa1ba20030d97d3a33f55836aa
Evangeline-Chang <79494629+Evangeline-Chang@users.noreply.github.com>
2023-04-02 13:37:25 -0700

    Add files via upload

---
+++ code/detoxify.py
@@ -0,0 +1,41 @@
+from detoxify import Detoxify
+import pandas as pd
+
+def detoxify(file):
+    df = pd.read_csv(file)
+
+    en_yet_detox = df.loc[(df['tika_lan_code'] == 'en') | (df['ggl_lan_code'] == 'en')]
+    for row in en_yet_detox.index:
+        try:
+            narrative = str(df.at[row,'Narrative'])
+            score = Detoxify('unbiased').predict(narrative)
+            df.at[row, 'toxicity'] = score.get('toxicity')
+            df.at[row, 'severe_toxicity'] = score.get('severe_toxicity') 
+            df.at[row, 'obscene'] = score.get('obscene') 
+            df.at[row, 'identity_attack'] = score.get('identity_attack')
+            df.at[row, 'insult'] = score.get('insult')
+            df.at[row, 'threat'] = score.get('threat')
+            df.at[row, 'sexual_explicit'] = score.get('sexual_explicit')
+        except Exception as e:
+            print(str(e))
+            print(row)
+            continue
+
+    transed_yet_detox = df.loc[(df['narrative_translation'].notna()) & (df['toxicity'].isna())]
+    for row in transed_yet_detox.index:
+        try:
+            narrative = str(df.at[row,'narrative_translation'])
+            score = Detoxify('unbiased').predict(narrative)
+            df.at[row, 'toxicity'] = score.get('toxicity')
+            df.at[row, 'severe_toxicity'] = score.get('severe_toxicity') 
+            df.at[row, 'obscene'] = score.get('obscene') 
+            df.at[row, 'identity_attack'] = score.get('identity_attack')
+            df.at[row, 'insult'] = score.get('insult')
+            df.at[row, 'threat'] = score.get('threat')
+            df.at[row, 'sexual_explicit'] = score.get('sexual_explicit')
+        except Exception as e:
+            print(str(e))
+            print(row)
+            continue
+
+    df.to_csv('pixstory_detoxify.csv')
