a04d278193150d86f53ab2934c5f2fa1c59a0640
Annie Chang <80128579+m-atcha@users.noreply.github.com>
2023-04-03 19:22:22 -0700

    deleted old detoxify code, added in observations script

---
+++ code/observations.py
@@ -0,0 +1,36 @@
+import pandas as pd
+from matplotlib import pyplot as plt
+import seaborn as sns
+
+pd.set_option('display.width', 400)
+pd.set_option('display.max_columns', 10)
+
+df = pd.read_csv('../data/pixstory/geo_df.csv')
+ld = pd.read_csv('../data/pixstory/pixstory_langdetect.csv')
+dt = pd.read_csv('../data/pixstory/pixstory_detoxify.csv')
+
+
+gender=df.value_counts(subset=['Gender','Location']).reset_index(name='count')
+print(gender)
+age=df.value_counts(subset=['Age','Location']).reset_index(name='count')
+print(age[35:])
+loc=df.value_counts('Location').reset_index(name='count')
+print(loc)
+lang= ld.value_counts('ggl_lan_code').reset_index(name='count')
+print(lang)
+sport=df.value_counts(subset=['sport_event','Location']).reset_index(name='count')
+print(sport[44:])
+fest=df.value_counts(subset=['Festivals','Location']).reset_index(name='count')
+print(fest[48:])
+
+dt["GLAAD"] = df["GLAAD"].astype(int)
+glaad_corr = dt["GLAAD"].corr(dt["toxicity"])
+print(glaad_corr)
+
+dt["ADL"] = df["ADL"].astype(int)
+adl_corr = dt["ADL"].corr(dt["toxicity"])
+print(adl_corr)
+
+dt["sarc"] = df["sarc"].astype(int)
+sarc_corr = dt["sarc"].corr(dt["toxicity"])
+print(sarc_corr)
